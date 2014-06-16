400e09ec223dd5b10902874ccc092de1ac09c0ff
netwindsky <netwindsky@163.com>
2014-06-16 19:10:41 +0800

    sqlite

--- java/org/lifeng/database/sqllite/SQLiteManager.java
+++ java/org/lifeng/database/sqllite/SQLiteManager.java
@@ -5,30 +5,65 @@
  */
 
 package org.lifeng.database.sqllite;
-import java.sql.Connection;
+import SQLite.*;
+
 /**
  *
  * @author Administrator
  */
 public class SQLiteManager {
-    
+    private Database db=new Database();
+    static final String strCreate ="create table user (userid integer primary key, username text)";  
+    static final String strInsert ="insert into user values (2,'James')";  
+    static final String strDisplay ="select * from user";  
     public SQLiteManager(){
         
     }
-    public void open(String path){
+    public int open(String path){
         try {
-            
-        } catch (Exception e) {
+            db.open(path, 0666);
+            return 1;
+        } catch (java.lang.Exception e) {
+            e.printStackTrace();
+            return -1;
         }
     }
     public void excuteNow(){
         
     }
-    public void excute(){
-        
+    public int excute(String sql){
+        try {
+            db.exec(sql,new TableFmt());
+            return 1;
+        } catch (java.lang.Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
     }
     public void close(){
         
     }
     
 }
+
+class TableFmt implements Callback{  
+    public void columns (String[] cols){  
+        System.out.println("columns");  
+        for (int i = 0; i < cols.length; i++) {  
+            System.out.println(cols[i]);  
+        }  
+    }  
+    public boolean newrow(String[] cols){  
+        System.out.println("newrow");  
+        for (int i = 0; i < cols.length; i++) {  
+            System.out.println(cols[i]);  
+        }  
+        return false;  
+    }  
+    public void types(String[] cols){  
+        System.out.println("types");  
+        for (int i = 0; i < cols.length; i++) {  
+            System.out.println(cols[i]);  
+        }  
+    }  
+}  
