80d78c6edf4cdbca2f0679de4a79902f68d7b245
netwindsky <netwindsky@163.com>
2014-06-16 18:56:07 +0800

    a

---
+++ java/org/lifeng/database/sqllite/SQLiteManager.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.lifeng.database.sqllite;
+import java.sql.Connection;
+/**
+ *
+ * @author Administrator
+ */
+public class SQLiteManager {
+    
+    public SQLiteManager(){
+        
+    }
+    public void open(String path){
+        try {
+            
+        } catch (Exception e) {
+        }
+    }
+    public void excuteNow(){
+        
+    }
+    public void excute(){
+        
+    }
+    public void close(){
+        
+    }
+    
+}
